2d8bb5f8e4849e9eb8c45438bfcdf82084a3e733
Raul <raulvilera@gmail.com>
2026-02-25 23:27:12 -0300

    fix: corrige variavel SUPABASE_URL no api para VITE_SUPABASE_URL

--- api/index.ts
+++ api/index.ts
@@ -11,7 +11,7 @@ const mp = new MercadoPagoConfig({
 });
 
 const supabase = createClient(
-    process.env.SUPABASE_URL || '',
+    process.env.VITE_SUPABASE_URL || process.env.SUPABASE_URL || '',
     process.env.SUPABASE_SERVICE_ROLE_KEY || ''
 );
 
